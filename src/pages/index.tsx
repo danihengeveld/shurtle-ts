40d93e38f6c6c48f5d97570b367834dbb8cf2620
Dani Hengeveld <dani10hengeveld@gmail.com>
2023-04-15 18:40:13 +0200

    more fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Button, buttonVariants } from "~/components/ui/button";
+import { Button } from "~/components/ui/button";
 import { SignOutButton, useUser } from "@clerk/nextjs";
 import { SignInButton } from "@clerk/nextjs";
 
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
           {user.isSignedIn && (
             <div className="flex flex-col items-center">
               <h3 className="tracking-light scroll-m-20 text-2xl font-semibold">
-                {`Welcome ${user.user.fullName}!`}
+                Welcome {user.user.fullName}!
               </h3>
               <p className="mt-6 text-lg leading-7">
                 This website is currently under construction. Come back later!
