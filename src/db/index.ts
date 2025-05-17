5d70d4de66a182ccc0f88351f021a53077dd18a4
Dani Hengeveld <dani10hengeveld@gmail.com>
2025-05-17 09:11:53 +0200

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,5 @@ import { config } from "dotenv";
 
 config({ path: "./.env.local" });
 
-const sql = neon(process.env.DATABASE_URL!)
+const sql = neon(process.env.POSTGRES_URL!)
 export const db = drizzle({ client: sql, schema, logger: true });
