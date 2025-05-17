3de1f2636cfb69eee6521753614c22ec0e06591b
Dani Hengeveld <dani10hengeveld@gmail.com>
2025-05-17 09:12:10 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
   tablesFilter: ["shurtle_*"]
 });
