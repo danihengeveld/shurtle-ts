5849f8a1a8e87498539ffdf5ee62cb2e81865c2c
Dani Hengeveld <dani10hengeveld@gmail.com>
2025-05-28 01:29:59 +0200

    ratelimit opening shurtles
    todo: creating shurtles

---
+++ src/lib/ratelimits.ts
@@ -0,0 +1,30 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+})
+
+export const rateLimits = {
+  openShurtle: {
+    limiter: new Ratelimit({
+      redis,
+      prefix: "ratelimit:openShurtle",
+      limiter: Ratelimit.slidingWindow(5, "1 m"), // 5 requests per minute
+      enableProtection: true,
+      analytics: true
+    }),
+    message: "You are opening too many Shurtles. Please wait a minute before trying again."
+  },
+  createShurtle: {
+    limiter: new Ratelimit({
+      redis,
+      prefix: "ratelimit:createShurtle",
+      limiter: Ratelimit.slidingWindow(5, "5 m"), // 5 requests per 5 minutes
+      enableProtection: true,
+      analytics: true
+    }),
+    message: "You are creating too many Shurtles. Please wait 5 minutes before trying again."
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { notFound } from 'next/navigation'
-import { NextResponse } from 'next/server'
-import { getUrlBySlug } from './lib/shurtles'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { ipAddress } from '@vercel/functions';
+import { NextRequest, NextResponse } from 'next/server';
+import { rateLimits } from './lib/ratelimits';
+import { getUrlBySlug } from './lib/shurtles';
 
-const isReservedRoute = createRouteMatcher(['/', '/dashboard(.*)', '/shurtle(.*)'])
-const isPublicRoute = createRouteMatcher(['/'])
+const isReservedRoute = createRouteMatcher(['/', '/dashboard(.*)', '/shurtle(.*)', '/not-found', '/api/(.*)'])
+const isPublicRoute = createRouteMatcher(['/', '/not-found', '/api/(.*)'])
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req, ctx) => {
   if (isReservedRoute(req)) {
     if (!isPublicRoute(req)) {
       await auth.protect()
@@ -15,17 +16,52 @@ export default clerkMiddleware(async (auth, req) => {
   }
 
   //If not a reserved route, we should try to match the slug and redirect.
+
+  // Check for rate limits first
+  const ip = ipAddress(req) || '127.0.0.1'; // Fallback to localhost if IP is not available
+
+  let headers: Headers | undefined = undefined;
+
+  if (ip) {
+    const { success, limit, remaining, reset, pending } = await rateLimits.openShurtle.limiter.limit(ip)
+    ctx.waitUntil(pending) // Explicitly wait for the rate limit check to complete
+
+    headers = createRateLimitHeaders(req, limit, remaining, reset)
+
+    if (!success) {
+      // If the rate limit is exceeded, we should return a 429 response with the appropriate headers.
+      return NextResponse.json({
+        error: rateLimits.openShurtle.message,
+      }, {
+        status: 429,
+        headers: headers,
+      });
+    }
+  }
+
   const slug = req.nextUrl.pathname.split('/').pop()
   if (slug && slug.length > 0) {
     const url = await getUrlBySlug(slug)
+
     if (url) {
-      return NextResponse.redirect(url)
+      return NextResponse.redirect(url, { headers: headers })
     } else {
       // If the slug is not found, we should return the not found page.
-      notFound()
+      return NextResponse.redirect(new URL('/not-found', req.url), { headers: headers })
     }
   }
 })
+
+function createRateLimitHeaders(req: NextRequest, limit: number, remaining: number, resetUnixTimestampMs: number) {
+  const headers = new Headers(req.headers)
+  headers.set('RateLimit-Limit', limit.toString())
+  headers.set('RateLimit-Remaining', remaining.toString())
+
+  const resetDelta = Math.floor((resetUnixTimestampMs - Date.now()) / 1000)
+  headers.set('RateLimit-Reset', resetDelta.toString())
+
+  return headers;
+}
 
 export const config = {
   matcher: [
