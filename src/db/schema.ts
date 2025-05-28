e2170cea813c69877ee0363643a7fac9dad721f8
Dani Hengeveld <dani10hengeveld@gmail.com>
2025-05-28 19:34:43 +0200

    start on schema changes for metadata

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,30 @@
 import { type InferInsertModel, type InferSelectModel } from "drizzle-orm";
-import { index, integer, pgTableCreator, timestamp, varchar } from "drizzle-orm/pg-core";
+import { bigserial, index, pgTableCreator, point, timestamp, varchar } from "drizzle-orm/pg-core";
 
 const pgTable = pgTableCreator((name) => `shurtle_${name}`)
 
 export const shurtles = pgTable("shurtles", {
 	slug: varchar("slug", { length: 255 }).primaryKey(),
 	url: varchar("url", { length: 255 }).notNull(),
-	hits: integer("hits").default(0).notNull(),
 	creatorId: varchar("creator_id", { length: 255 }).notNull(),
-	createdAt: timestamp("created_at", { withTimezone: true, mode: 'date', precision: 0 }).defaultNow().notNull(),
-	lastHitAt: timestamp("last_hit_at", { withTimezone: true, mode: 'date', precision: 0 })
+	createdAt: timestamp("created_at", { withTimezone: true, mode: 'date', precision: 0 }).defaultNow().notNull()
 }, (table) => [
 	index("creator_id_idx").on(table.creatorId)
-]);
+])
+
+export const shurtleHits = pgTable("hits", {
+	id: bigserial("id", { mode: "bigint" }).primaryKey(),
+	slug: varchar("slug", { length: 255 }).notNull().references(() => shurtles.slug, { onDelete: 'cascade' }),
+	at: timestamp("at", { withTimezone: true, mode: 'date', precision: 0 }).defaultNow().notNull(),
+	fromCountry: varchar("from_country", { length: 255 }),
+	fromCity: varchar("from_city", { length: 255 }),
+	fromLocation: point("from_location", { mode: 'xy' })
+}, (table) => [
+	index("slug_idx").on(table.slug)
+])
 
 export type Shurtle = InferSelectModel<typeof shurtles>
 export type ShurtleInsert = InferInsertModel<typeof shurtles>
+
+export type ShurtleHit = InferSelectModel<typeof shurtleHits>
+export type ShurtleHitInsert = InferInsertModel<typeof shurtleHits>
