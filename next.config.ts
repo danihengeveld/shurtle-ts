2c19ba9a60220b11cc53c85da51fe29f1dbdf0b4
Dani Hengeveld <dani.hengeveld@infosupport.com>
2025-12-05 16:38:50 +0100

    enable react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  cacheComponents: true
+  cacheComponents: true,
+  reactCompiler: true
 };
 
 module.exports = nextConfig;
